406b08110adf157925387f68f8f4e12faf7b5695
Sean Grove <sean@fakecoolguys.com>
2010-10-19 11:06:32 -0700

    Refactored board into a class.

--- battleship.rb
+++ battleship.rb
@@ -27,66 +27,93 @@
   ===============================
   [HIGH] Keep ships from overlapping
   [LOW] Notify when single ship has been sunk
+  [LOW] Keep track of how many undamaged pieces on a board
   [HIGH] Check when board has no undamaged ship pieces left [DONE]
+  [HIGH] Prevent ships from being placed off the map
 
 =end
 
-def new_board
-  return [
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0]
-         ]
-end
+class Board
+  attr_reader :board
 
-def still_alive(board)
-  alive = false
-  board.each do |row|
-    alive = true if row.member? 1
+  def initialize
+    @board = new_board
   end
 
-  return alive
-end
+  def new_board
+    return [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+           ]
+  end
 
-def show_board(board)
-  board.each { |row| puts row.inspect }
-end
+  def alive?
+    live_count != 0
+  end
 
-def place_value(board, x, y, value)
-  board[y][x] = value
-end
+  def live_count
+    count = 0 
 
-def place_ship(board, orientation, x, y, length)
-  # TODO: Probably a nicer way to handle orientation,
-  #       don't care for the conditional here.
-  if "horizontal" == orientation
-    length.times do |offset|
-      place_value(board, x + offset, y, 1)
+    @board.each do |row|
+      row.each do |column|
+        count += 1 if 1 == column
+      end
     end
-  elsif "vertical" == orientation
-    length.times do |offset|
-      place_value(board, x, y + offset, 1)
+
+    return count
+  end
+
+  def show
+    puts "------------------------------"
+    @board.each { |row| puts row.inspect }
+    puts
+  end
+
+  def place_value(x, y, value)
+    @board[y][x] = value
+  end
+
+  def place_ship(orientation, x, y, length)
+    # TODO: Probably a nicer way to handle orientation,
+    #       don't care for the conditional here.
+    if "horizontal" == orientation
+      length.times do |offset|
+        place_value(x + offset, y, 1)
+      end
+    elsif "vertical" == orientation
+      length.times do |offset|
+        place_value(x, y + offset, 1)
+      end
     end
   end
-end
 
-def value_at(board, x, y)
-  return board[y][x]
-end
-
-def bomb(board, x, y)
-  if 1 == value_at(board, x, y)
-    place_value(board, x, y, 2)
-    return true
+  def value_at(x, y)
+    return @board[y][x]
   end
 
-  return false
+  def bomb(x, y)
+    if 1 == value_at(x, y)
+      place_value(x, y, 2)
+      return true
+    end
+
+    return false
+  end
 end
+
+board = Board.new
+board.show
+board.place_ship("horizontal", 2, 5, 1)
+board.show
+board.bomb(2, 5)
+board.show
+puts board.alive?
