d57a06da7f6a846e9f373a5760f0b059e0a73635
Sean Grove <sean@fakecoolguys.com>
2010-10-19 10:55:56 -0700

    Basic verbs in place. Next: REfactor into a class

--- battleship.rb
+++ battleship.rb
@@ -2,6 +2,11 @@
   Board in row-major format
   0 indexed
 
+  Legend:
+  0 - Sea
+  1 - Ship
+  2 - Damaged ship
+
   bomb(x, y) | ex: -> bomb(4, 1)
   ------------------------------
   [
@@ -18,12 +23,18 @@
   [0, 0, 0, 0, 0, 0, 0]
   ]
 
+  TODO:
+  ===============================
+  [HIGH] Keep ships from overlapping
+  [LOW] Notify when single ship has been sunk
+  [HIGH] Check when board has no undamaged ship pieces left [DONE]
+
 =end
 
 def new_board
   return [
-          [1, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 10, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0],
@@ -36,11 +47,46 @@ def new_board
          ]
 end
 
+def still_alive(board)
+  alive = false
+  board.each do |row|
+    alive = true if row.member? 1
+  end
+
+  return alive
+end
+
+def show_board(board)
+  board.each { |row| puts row.inspect }
+end
+
+def place_value(board, x, y, value)
+  board[y][x] = value
+end
+
+def place_ship(board, orientation, x, y, length)
+  # TODO: Probably a nicer way to handle orientation,
+  #       don't care for the conditional here.
+  if "horizontal" == orientation
+    length.times do |offset|
+      place_value(board, x + offset, y, 1)
+    end
+  elsif "vertical" == orientation
+    length.times do |offset|
+      place_value(board, x, y + offset, 1)
+    end
+  end
+end
 
 def value_at(board, x, y)
   return board[y][x]
 end
 
-puts value_at(new_board, 0, 0)
-puts value_at(new_board, 4, 1)
-puts value_at(new_board, 4, 2)
+def bomb(board, x, y)
+  if 1 == value_at(board, x, y)
+    place_value(board, x, y, 2)
+    return true
+  end
+
+  return false
+end
